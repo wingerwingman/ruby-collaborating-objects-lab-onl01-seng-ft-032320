01ad26457fe527ee889b0742f0b74e469ec640cc
Justin Winger <justinwinger4@gmail.com>
2020-04-06 18:37:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
   
   def files
     files = []
-    Dir.new(self.files).each do |files|
+    Dir.new(self.files).each do |file|
       files << file if file.lenght > 4
     end
     files
